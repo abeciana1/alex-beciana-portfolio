edd66adfbd4b577f4f4a6335d9092d9c9aa1a3a0
Alex <alex.beciana@gmail.com>
2024-11-03 16:31:48 -0700

    Added some styling, unordered list for navigation links, and name

--- app/(components)/_navigation/NavBar/index.tsx
+++ app/(components)/_navigation/NavBar/index.tsx
@@ -1,6 +1,19 @@
+import Link from 'next/link'
+
 const NavBar = () => {
   return (
-    <nav></nav>
+    <nav className="font-tiltNeon px-5 md:px-10 py-3">
+      <Link
+        data-testid="my-name"
+        href="/"
+        className="text-2xl"
+      >
+        Alex Beciana
+      </Link>
+      <ul>
+        
+      </ul>
+    </nav>
   )
 }
 
