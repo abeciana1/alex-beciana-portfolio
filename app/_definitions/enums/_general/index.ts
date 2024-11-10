7ff040fe949898e28dcf801cbb9064b55745c942
Alex <alex.beciana@gmail.com>
2024-11-10 11:25:58 -0700

    Created and exported radius and color enums

---
+++ app/_definitions/enums/_general/index.ts
@@ -0,0 +1,15 @@
+export enum RadiusE {
+  SM = 'sm',
+  MD = 'md',
+  LG = 'lg',
+  FULL = 'full'
+}
+
+export enum ColorE {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  SUCCESS = 'success',
+  WARNING = 'warning',
+  DANGER = 'danger',
+  FOREGROUND = 'foreground'
+}
