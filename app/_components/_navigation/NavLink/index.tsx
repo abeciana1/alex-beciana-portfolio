cf72915c05bf452942f126017d50451d57fcef0e
Alex <alex.beciana@gmail.com>
2024-11-10 10:23:26 -0700

    Added active state classname styles

--- app/_components/_navigation/NavLink/index.tsx
+++ app/_components/_navigation/NavLink/index.tsx
@@ -1,17 +1,26 @@
+'use client';
 import { NavLinkI } from '@/app/_definitions/interfaces/_navigation';
-import Link from 'next/link'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import cx from 'classnames';
 
-const NavLink = ({
-  linkText,
-  href
-}: NavLinkI) => {
+const NavLink = ({ linkText, href }: NavLinkI) => {
+  const pathname = usePathname();
+
   return (
     <li>
-      <Link href={href} prefetch>
+      <Link
+        href={href}
+        prefetch
+        className={cx('text-2xl', {
+          ['underline text-primary']: pathname === href,
+          ['text-foreground']: pathname !== href,
+        })}
+      >
         {linkText}
       </Link>
     </li>
-  )
+  );
 };
 
 export default NavLink;
