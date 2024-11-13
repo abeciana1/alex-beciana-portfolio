5d79af778735f5087ec4b71eea6f2eff3f04814b
Alex <alex.beciana@gmail.com>
2024-11-12 19:11:16 -0700

    Created PressEvent and OnPress types

--- app/_definitions/types/_buttons/index.ts
+++ app/_definitions/types/_buttons/index.ts
@@ -1,3 +1,9 @@
+import { MouseEvent, TouchEvent } from 'react';
+
+export type PressEvent = MouseEvent<HTMLButtonElement> | TouchEvent<HTMLButtonElement>;
+
 export type MouseEventFuncT = (event: MouseEvent) => void;
 
 export type OnClickNoReturnFuncT = () => void;
+
+export type OnPress = (event: PressEvent) => void;
