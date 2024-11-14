ec9fc7f158d4d17f92f5cbf3e2504d15c3452912
Alex <alex.beciana@gmail.com>
2024-11-13 21:13:51 -0700

    Added types to Button interface

--- app/_definitions/interfaces/_buttons/index.ts
+++ app/_definitions/interfaces/_buttons/index.ts
@@ -1,18 +1,21 @@
-import { OnPress } from '@/app/_definitions/types/_buttons'
-import { RadiusE, ColorE } from '@/app/_definitions/enums/_general'
-import { ElementType } from "react";
+import { OnPress, VariantT } from '@/app/_definitions/types/_buttons';
+import { RadiusE, ColorE } from '@/app/_definitions/enums/_general';
+import { ElementType } from 'react';
+// import { ColorT } from '@/app/_definitions/types/_general'
 
 export interface ButtonI {
   buttonText?: string;
   onClick: OnPress;
   testId?: string;
+  textColor?: ColorE;
+  color: ColorE;
+  variant?: VariantT
 }
 
 export interface ButtonIconI extends ButtonI {
   buttonIcon: ElementType;
   iconOnly?: boolean;
   radius: RadiusE;
-  color: ColorE;
   startIcon?: boolean;
   endIcon?: boolean;
   iconSize: number;
