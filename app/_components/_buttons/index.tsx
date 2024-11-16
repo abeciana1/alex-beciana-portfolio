36d35281be38dbb7b3092b4441d203cd23864b8b
Alex <alex.beciana@gmail.com>
2024-11-15 22:04:32 -0700

    Added styling for icon button

--- app/_components/_buttons/index.tsx
+++ app/_components/_buttons/index.tsx
@@ -1,8 +1,10 @@
-import { Button } from "@nextui-org/react";
-import { ButtonIconI } from '@/app/_definitions/interfaces/_buttons'
+import { Button } from '@nextui-org/react';
+import { ButtonIconI } from '@/app/_definitions/interfaces/_buttons';
+import cx from 'classnames'
+import { ColorE } from '@/app/_definitions/enums/_general'
 
 export const IconButton = ({
-  buttonText,
+  buttonText = '',
   onClick,
   buttonIcon,
   iconOnly = false,
@@ -12,20 +14,60 @@ export const IconButton = ({
   endIcon = false,
   iconSize,
   testId = '',
+  textColor,
+  variant = "solid"
 }: ButtonIconI) => {
-  const Icon = buttonIcon
+  const Icon = buttonIcon;
+  const iconStyle = {
+    color: textColor
+  }
+  // todo remove color attr -> use classnames
+  // todo add border class name styles
+  // todo work on 
   return (
     <>
-      {(buttonIcon && startIcon && (endIcon === false)) &&
-        <Button data-testid={testId} onClick={onClick} isIconOnly={iconOnly} color={color} radius={radius} startContent={<Icon size={iconSize} />}>
-          {(buttonText && !iconOnly) && buttonText}
+      {buttonIcon && startIcon && endIcon === false && (
+        <Button
+          variant={variant}
+          data-testid={testId}
+          onClick={onClick}
+          isIconOnly={iconOnly}
+          radius={radius}
+          startContent={<Icon style={iconStyle} size={iconSize} />}
+          className={cx({
+            ['bg-background']: color === ColorE.BACK,
+            ['bg-foreground']: color === ColorE.FORE,
+            ['bg-primary']: color === ColorE.PRIMARY,
+            ['bg-secondary']: color === ColorE.SECONDARY,
+            ['bg-danger']: color === ColorE.DANGER,
+            ['bg-warning']: color === ColorE.WARNING,
+            ['bg-success']: color === ColorE.SUCCESS
+          })}
+        >
+          {buttonText && !iconOnly && buttonText}
         </Button>
-      }
-      {(buttonIcon && endIcon && (startIcon === false)) &&
-        <Button data-testid={testId} onClick={onClick} isIconOnly={iconOnly} color={color} radius={radius} endContent={<Icon size={iconSize} />}>
-          {(buttonText && !iconOnly) && buttonText}
+      )}
+      {buttonIcon && endIcon && startIcon === false && (
+        <Button
+          variant={variant}
+          data-testid={testId}
+          onClick={onClick}
+          isIconOnly={iconOnly}
+          radius={radius}
+          endContent={<Icon size={iconSize} />}
+          className={cx({
+            ['bg-background']: color === ColorE.BACK,
+            ['bg-foreground']: color === ColorE.FORE,
+            ['bg-primary']: color === ColorE.PRIMARY,
+            ['bg-secondary']: color === ColorE.SECONDARY,
+            ['bg-danger']: color === ColorE.DANGER,
+            ['bg-warning']: color === ColorE.WARNING,
+            ['bg-success']: color === ColorE.SUCCESS
+          })}
+        >
+          {buttonText && !iconOnly && buttonText}
         </Button>
-      }
+      )}
     </>
-  )
-}
+  );
+};
