ef19a6f20ddfe0aa7de6a6773271e148332f0fbb
abeciana1 <alex.beciana@gmail.com>
2024-11-29 12:58:52 -0700

    Created Heading1 component

---
+++ app/_components/_styled/_headings/index.tsx
@@ -0,0 +1,24 @@
+import { HeadingI } from '@/app/_definitions/interfaces/_styled/_headings'
+import { ColorE } from '@/app/_definitions/enums/_general'
+import cx from 'classnames'
+
+export const Heading1 = ({
+  text,
+  color,
+  underline = false
+}: HeadingI) => {
+  return (
+    <h1
+      className={cx('text-5xl', {
+        ['text-background']: color === ColorE.BACK,
+        ['text-foreground']: color === ColorE.FORE,
+        ['text-primary']: color === ColorE.PRIMARY,
+        ['text-secondary']: color === ColorE.SECONDARY,
+        ['text-danger']: color === ColorE.DANGER,
+        ['text-warning']: color === ColorE.WARNING,
+        ['text-success']: color === ColorE.SUCCESS,
+        ['underline']: underline
+      })}
+    >{ text }</h1>
+  )
+}
