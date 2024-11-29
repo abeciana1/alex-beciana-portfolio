489ce143356c186b2bb51040de0cd0542dd4db1c
abeciana1 <alex.beciana@gmail.com>
2024-11-29 12:07:38 -0700

    Added test for icon button

--- app/_components/_buttons/__test__/index.test.tsx
+++ app/_components/_buttons/__test__/index.test.tsx
@@ -1,2 +1,29 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { IconButton } from '@/app/_components/_buttons'
+import { ColorE, RadiusE } from '@/app/_definitions/enums/_general'
+import { MdFavorite } from "react-icons/md";
+
+describe('Buttons', () => {
+  describe('IconButton', () => {
+    beforeEach(() => {
+      render(
+        <IconButton
+          buttonText="Test Button"
+          onClick={() => {}}
+          buttonIcon={MdFavorite}
+          iconSize={24}
+          color={ColorE.BACK}
+          radius={RadiusE.FULL}
+          testId="test-icon-button"
+          startIcon
+        />
+      )
+    })
+    it('renders the button text', () => {
+      const button = screen.getByTestId('test-icon-button')
+      expect(button.textContent).toBe('Test Button')
+      expect(button).toBeInTheDocument()
+    })
+  })
+})
