ca37b8e83a0ae2695b697da48e8efb345c36ea48
abeciana1 <alex.beciana@gmail.com>
2024-11-29 20:17:00 -0700

    Created TwoColumnHero interface

---
+++ app/_definitions/interfaces/_sections/index.ts
@@ -0,0 +1,13 @@
+import { HeadingLevelT } from '@/app/_definitions/types/_general'
+import { ImageI } from '@/app/_definitions/interfaces/_general'
+
+export interface TwoColumnHeroI {
+  headingLevel: HeadingLevelT;
+  headingText: string;
+  subheadingText: string;
+  bodyText: string;
+  gradientBlur?: boolean;
+  reverseOrder?: boolean;
+  children: React.ReactNode;
+  image: ImageI;
+}
