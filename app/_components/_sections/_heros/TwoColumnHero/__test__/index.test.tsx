294331e740797f9ac58cb0aace2dfc05895991a1
abeciana1 <alex.beciana@gmail.com>
2024-12-01 12:24:30 -0700

    Adjusted tests

--- app/_components/_sections/_heros/TwoColumnHero/__test__/index.test.tsx
+++ app/_components/_sections/_heros/TwoColumnHero/__test__/index.test.tsx
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import TwoColumnHero from '@/app/_components/_sections/_heros/TwoColumnHero'
-import { ColorE, RadiusE } from '@/app/_definitions/enums/_general'
-import { MdFavorite } from "react-icons/md";
-import { IconButton } from '@/app/_components/_buttons'
+import TwoColumnHero from '@/app/_components/_sections/_heros/TwoColumnHero';
+import { ColorE, RadiusE } from '@/app/_definitions/enums/_general';
+import { MdFavorite } from 'react-icons/md';
+import IconButton from '@/app/_components/_buttons/IconButton';
 
 describe('TwoColumnHero', () => {
   beforeEach(() => {
@@ -15,11 +15,11 @@ describe('TwoColumnHero', () => {
         bodyText="I'm Alex Beciana, a passionate software engineer with a background in user experience design. I've recently launched my first web application called 'Project Kanban', which aims to help you manage your projects efficiently."
         gradientBlur={false}
         image={{
-          src: '@/public/profile-callout-edited.webp',
+          src: '/profile-callout-edited.webp',
           alt: 'test image',
-          className: "test-image",
+          className: 'test-image',
           width: 500,
-          height: 400
+          height: 400,
         }}
         reverseOrder={false}
       >
@@ -34,29 +34,31 @@ describe('TwoColumnHero', () => {
           startIcon
         />
       </TwoColumnHero>
-    )
-  })
+    );
+  });
   it('should render the heading', () => {
     const heading = screen.getByRole('heading', {
-      level: 1
-    })
-    expect(heading).toBeInTheDocument()
-    expect(heading).toHaveTextContent('My Profile')
-  })
+      level: 1,
+    });
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('My Profile');
+  });
   it('should render the subheading', () => {
-    const subHeading = screen.getByText('A Brief Overview')
-    expect(subHeading).toBeInTheDocument()
-  })
+    const subHeading = screen.getByText('A Brief Overview');
+    expect(subHeading).toBeInTheDocument();
+  });
   it('should render the body text', () => {
-    const bodyText = screen.getByText("I'm Alex Beciana, a passionate software engineer with a background in user experience design. I've recently launched my first web application called 'Project Kanban', which aims to help you manage your projects efficiently.")
-    expect(bodyText).toBeInTheDocument()
-  })
+    const bodyText = screen.getByText(
+      "I'm Alex Beciana, a passionate software engineer with a background in user experience design. I've recently launched my first web application called 'Project Kanban', which aims to help you manage your projects efficiently."
+    );
+    expect(bodyText).toBeInTheDocument();
+  });
   it('should render image', () => {
-    const image = screen.getByAltText('test image')
-    expect(image).toBeInTheDocument()
-  })
+    const image = screen.getByAltText('test image');
+    expect(image).toBeInTheDocument();
+  });
   it('should render the children', () => {
-    const button = screen.getByTestId('test-icon-button')
-    expect(button).toBeInTheDocument()
-  })
-})
+    const button = screen.getByTestId('test-icon-button');
+    expect(button).toBeInTheDocument();
+  });
+});
