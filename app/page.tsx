39a9d1da9385a2907e243001bfdf33b24acc93f6
abeciana1 <alex.beciana@gmail.com>
2024-12-01 17:08:14 -0700

    Added expand button and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use server';
 import TwoColumnHero from '@/app/_components/_sections/_heros/TwoColumnHero';
 import { TiSocialLinkedin } from 'react-icons/ti';
-// import { AiOutlineGithub, AiOutlineBehance } from 'react-icons/ai';
+import { AiOutlineGithub, AiOutlineBehance } from 'react-icons/ai';
 import ExpandBtnLink from './_components/_links/ExpandBtn';
 import { ColorE } from './_definitions/enums/_general';
 
@@ -23,10 +23,26 @@ export default async function Home() {
       >
         <div className="flex pb-8 space-x-4 md:space-y-0 pt-5 flex-row relative items-center">
           <ExpandBtnLink
-            icon={<TiSocialLinkedin strokeWidth='1' className='h-7 w-7 mx-auto fill-background' />}
+            buttonIcon={<TiSocialLinkedin strokeWidth='1' className='h-7 w-7 mx-auto fill-background' />}
             linkText='LinkedIn'
             href="https://www.linkedin.com/in/alexbeciana"
             ariaLabel="Link to Alex Beciana LinkedIn profile"
+            addClass="hover:w-32"
+            color={ColorE.FORE}
+          />
+          <ExpandBtnLink
+            buttonIcon={<AiOutlineGithub strokeWidth='1' className='h-7 w-7 fill-background' />}
+            linkText='GitHub'
+            href="https://github.com/abeciana1"
+            ariaLabel="Link to Alex Beciana GitHub profile"
+            addClass="hover:w-32"
+            color={ColorE.FORE}
+          />
+          <ExpandBtnLink
+            buttonIcon={<AiOutlineBehance strokeWidth='1' className='h-7 w-7 mx-auto fill-background' />}
+            linkText='Behance'
+            href="https://www.behance.net/alexbeciana"
+            ariaLabel="Link to Alex Beciana Behance profile"
             addClass="hover:w-32"
             color={ColorE.FORE}
           />
